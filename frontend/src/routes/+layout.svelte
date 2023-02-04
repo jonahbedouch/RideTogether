0c9d3662a3d605172f927d38dc53307448c35436
Jonah Bedouch <jonah@bedouch.net>
2023-02-04 10:46:18 -0800

    Implement location tracking and use map view to add geo pointer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
 	import 'mapbox-gl/dist/mapbox-gl.css';
+	import { onMount } from 'svelte';
 	import '../app.css';
+	import { locationStore } from '../stores/location';
+
+    onMount(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(locationStore.success, locationStore.error ,
+                {enableHighAccuracy: false, timeout: 5000, maximumAge: 10});
+        }
+    })
 </script>
 
 <slot />
