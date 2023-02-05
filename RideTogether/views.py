1f026fdaa32e70684048a875ef9f11432f48581b
Kyle Luo <kluo15@ucsc.edu>
2023-02-05 01:54:32 -0800

    more bugfixes

--- RideTogether/views.py
+++ RideTogether/views.py
@@ -135,7 +135,7 @@ class session_find_api_view(APIView):
         print(Session.objects.all())
         possible = Session.objects.exclude(passengers_max=0)
         origin = latlong_to_bucket(lat1, long1)
-        possible = [x for x in possible if (timestart - 3600 < x.timestamp < timestart + 3600) and (x.passengers_max-peopleamt >=0)]
+        possible = [x for x in possible if (timestart - 360000 < x.timestamp < timestart + 360000) and (x.passengers_max-peopleamt >=0)]
         dest = latlong_to_bucket(lat2, long2)
         print(possible)
         print(origin, dest)
